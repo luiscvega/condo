2eb33c072c13d9aabe11635cb9f0aa77632ca57a
Gabriel Jaldon <gjaldon85@gmail.com>
2014-05-26 00:06:09 +0800

    Add flash alerts in login JS

--- public/js/main.js
+++ public/js/main.js
@@ -3,24 +3,33 @@
 var app = angular.module('condoApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'login',
-      controller: 'LoginCtrl'
-    });
+  $routeProvider.when('/', {
+    templateUrl: 'templates/index',
+    controller: 'TenantCtrl'
+  })
+
+  $routeProvider.when('/login', {
+    templateUrl: 'templates/login',
+    controller: 'SessionCtrl'
+  });
 });
 
-app.controller('LoginCtrl', function($scope, $http) {
-  $scope.user = {email: '', password: ''};
-
+app.controller('SessionCtrl', function($scope, $http, $location, $rootScope) {
   $scope.submitLogin = function() {
     $http.post('/login', $scope.user).
       success(function(data, status, headers, config){
-        console.log('Logged in' + data);
+        data.alert = 'success';
+        $rootScope.flash = data;
+        $location.path("/");
       }).
       error(function(data, status, headers, config) {
-        console.log(data);
+        data.alert = 'danger';
+        $rootScope.flash = data;
       });
   };
+
+});
+
+app.controller('TenantCtrl', function($scope, $http) {
 
 });
--- routes/admin_routes.rb
+++ routes/admin_routes.rb
@@ -1,7 +1,2 @@
 class AdminRoutes < Cuba
-  define do
-    on "login" do
-      res.write({ hello: "my message" }.to_json)
-    end
-  end
 end
